7745ac1913984e7b69e9984d7c3e733aaa060dd3
Charls Cabangon <charlsmercabangon@gmail.com>
2026-08-21 19:02:17 +0800

    chore: update details

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,7 +24,7 @@ export default defineNuxtConfig({
                 { name: 'viewport', content: 'width=device-width, initial-scale=1' },
                 {
                     name: 'description',
-                    content: 'Your journey with Dmi awaits. Board now.',
+                    content: 'Your journey with Dmi awaits. Board now!',
                 },
             ],
         },
