96e5c98acb3294ef03ca266ad4bfbc8247ce8eaa
Charls Cabangon <charlsmercabangon@gmail.com>
2026-08-22 16:45:53 +0800

    chore(ui): component and asset updates

--- app/components/ui/UiCard.vue
+++ app/components/ui/UiCard.vue
@@ -9,9 +9,9 @@ defineProps<Props>();
 </script>
 
 <template>
-    <article class="h-16 w-xs bg-primary-hover flex items-center gap-2 p-2 rounded-xl ring ring-primary-hover shadow-md">
-        <div class="h-12 flex items-center justify-center aspect-square bg-primary-bg p-2 rounded-lg">
-            <img :src="icon" :alt="title" class="size-7" />
+    <article class="h-16 w-xs bg-primary-hover flex items-center gap-3 p-2 rounded-xl border-2 border-white/90 shadow-md inner-shadow">
+        <div class="h-12 flex items-center justify-center aspect-square bg-primary-bg border-2 border-white/50 p-2 rounded-lg shadow-inner">
+            <img :src="icon" :alt="title" class="size-6" />
         </div>
 
         <div class="space-y-1 pr-3">
